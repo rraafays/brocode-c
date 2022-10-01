1abf5b34670756f8ee5f0f3ad604282aa075bd82
raf <rraf@tuta.io>
2022-10-01 10:05:48 +0100

    update: completed task 7

---
+++ 7.constants/constants.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  const float PI = 3.14159; // constants are fixed values that cannot be altered by the program during its execution
+
+  printf("pi: %.5f", PI);
+
+  return 0;
+}
